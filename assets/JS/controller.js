0515ed4cd9fe2ded680c9c9f7557011cc176ed59
Guilherme-lgs <melloguilerme38@gmail.com>
2025-04-22 16:30:27 -0300

    :perf: adicionei o modal em todo escopo de eventos sugeridos

--- assets/JS/controller.js
+++ assets/JS/controller.js
@@ -2,9 +2,15 @@ const botao = document.getElementById("modal_btn");
 const modal = document.getElementById("m_modal");
 
 function m_modal() {
-    modal.showModal();
+    modal.showModal(); // Abre o modal
 }
 
 function fecharModal() {
-    modal.close();
+    modal.close(); // Fecha o modal
 }
+
+// Fecha o modal ao pressionar "Esc"
+modal.addEventListener("cancel", (event) => {
+    event.preventDefault(); // Impede o comportamento padrão de fechar
+    modal.close();
+});
